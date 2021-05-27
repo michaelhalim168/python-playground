6058d68e18534e1437d6886c61ebb1c2a29ad374
Michael Halim <michaelhalim168@gmail.com>
2021-05-27 00:25:28 -0700

    Added Python Challenges

---
+++ python-challenges/01_fizzbuzz.py
@@ -0,0 +1,15 @@
+'''
+Challenge #1: FizzBuzz Challenge
+Date: May 26, 2021
+
+'''
+
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
